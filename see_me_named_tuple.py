cf5c80839b46ba402e16301027f5eb8d95e7836a
julius-alexander <jgarzonferrer@gmail.com>
2023-08-01 21:14:17 -0400

    forgot to commit this file... oops

--- see_me_named_tuple.py
+++ see_me_named_tuple.py
@@ -21,3 +21,17 @@ print(color4)
 print(color1.r)
 print(color1.g)
 print(color1.b)
+
+# * Example call for assignment:
+# my_abra = Abra()
+# defaults to level 1, no EVs, no IVs, no nature, no item, lvl. 1 moveset
+#
+# my_abra = Abra(level=5,
+#               evs={'atk': 252, 'spe': 252, 'hp': 4},
+#               ivs={'def': 30},
+#               nature='adamant',
+#               item='life orb',
+#               moves=('psychic', 'fire punch', 'thunder punch', 'ice punch')
+#           )
+#
+# This one assigns certain values to certain fields, and the rest are default
